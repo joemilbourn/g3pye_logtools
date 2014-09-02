c4f3d4ad03148e266a6171c129780b7f35c1d4a4
Joe Milbourn <joe@milbourn.org.uk>
2014-09-02 21:16:01 +0100

    removed some unused files

--- clint_test.py
+++
@@ -1,7 +0,0 @@
-import clint
-
-print clint.args.grouped
-print clint.args.grouped['-f']
-print dir(clint.args.grouped['-f'])
-print str(clint.args.grouped['-f'])
-print clint.args.grouped['-f'].last
